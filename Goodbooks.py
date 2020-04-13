43b01effd30065444cf3ee04e56a3029e5cf7f36
sultan khan <sultanmkhan@outlook.com>
2020-04-13 18:59:50 +0100

    just coded a search method yet to communicate with APIs

--- Goodbooks.py
+++ Goodbooks.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app= Flask(__name__)
 @app.route('/')
 def login():
     return render_template('login.html')
-if __name__== '__main__' :
-    app.run(debug=True)
+
+@app.route('/search/<keyword>', methods=['GET'])
+def search(keyword):
+    response= {keyword: "Cound not find it check the spellings and try again"}
+    if keyword=="hi":
+        response="hi"
+        
+    return jsonify(response)
+
+
+if __name__== '__main__':
+    app.run(host='localhost', port=5000, debug=True)
