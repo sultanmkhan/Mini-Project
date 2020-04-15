217d9615d50231e7621c8b2343cfd6c72a288ddc
sultan khan <sultanmkhan@outlook.com>
2020-04-15 15:42:04 +0100

    User password is being converted to hash with salt so it can be compared with stored value in Cassandra table

--- Goodbooks.py
+++ Goodbooks.py
@@ -1,17 +1,35 @@
 from flask import Flask, render_template, jsonify, Response
+import xml.etree.ElementTree as ET
 import requests
+import hashlib
 
 app= Flask(__name__)
 @app.route('/')
-def login():
-    return render_template('login.html')
+def index():
+   return render_template('index.html')
+
+@app.route('/<username>/<password>', methods=['GET', 'POST'])
+def login(username, password):
+    salt='5gz'
+    password =password+salt
+    password= hashlib.md5(password.encode())
+    password= password.hexdigest()
+    if password=='24e2a20dcb8d414d13a21209ad26624e':
+        print('hi sultan')
+        return {username:'welcome'}
+    else:
+        print('wrong password')
+        return {password: 'hi there'}
 
 @app.route('/search/<keyword>', methods=['GET'])
 def search(keyword=0):
    if keyword!=0:
          url="https://www.goodreads.com/search/index.xml?key=iLbLcwF5ca4oLVjiCdn4A&q=keyword"
          response= requests.get(url).content
-         return Response(response)
+         root= ET.fromstring(response)
+         for i in root.iter('title'):
+             print(i.attrib)
+         return root[1][3].text
    else:
        return jsonify("Could not find it check the spellings and try again")
 
